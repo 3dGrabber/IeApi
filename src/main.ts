6932c8052a3333950840d75c8edecae9f3d01f40
ig <ig ig@devnullmail.com>
2022-03-01 11:05:24 +0100

    add more docs

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import {isDefined} from "./utils";
 import fetch from "node-fetch";
 import {vrmUser, vrmPassword} from "../secrets";
 import {IncomingMessage, ServerResponse} from "http";
+import * as http from "http";
 
 const port = 8080;
 
+// https://docs.victronenergy.com/vrmapi/overview.html
 const vrmApi = "https://vrmapi.victronenergy.com/v2/"
 
 type UserToken = { idUser: number, token: string }
@@ -19,10 +21,14 @@ async function vrmGet(): Promise<void>
     };
 
     const body = JSON.stringify(credentials);
+
+    console.log("logging into VRM")
+
     const response = await fetch(vrmApi + "auth/login", {method: "POST", body});
     const json = await response.json() as UserToken;
-}
 
+    console.log(`got token ${json.token}`)
+}
 
 
 function serve(request: IncomingMessage, response: ServerResponse): void
@@ -31,7 +37,7 @@ function serve(request: IncomingMessage, response: ServerResponse): void
 
     if (isDefined(url))
     {
-        const parsed = new URL(url, `https://${request.headers.host}`);
+        const parsed = new URL(url, `https://${request.headers.host}`);  // way to parse path/queryString from host header
         response.write(parsed.pathname);
     }
 
@@ -40,10 +46,8 @@ function serve(request: IncomingMessage, response: ServerResponse): void
 }
 
 vrmGet();
-//
-// console.log(`starting server on port ${port}`)
-//
-// http.createServer(serve)
-//     .listen(port);
+
+console.log(`starting server on port ${port}`)
+http.createServer(serve).listen(port);
 
 
